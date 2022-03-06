7bef2c7f8205582e8c590cfc0afda689f3ab75ac
saksham37 <sharma.ibn7@gmail.com>
2022-03-07 01:30:36 +0530

    Add setState

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,4 +1,6 @@
+// import { type } from '@testing-library/user-event/dist/type';
 import React from 'react';
+
 
 class CartItem extends React.Component{
     // we need to make a constructor to define the state
@@ -7,7 +9,37 @@ class CartItem extends React.Component{
         this.state = {
             title: 'Mobile Phone',
             price: '69',
-            qty: '1'
+            qty: 1
+        }
+        this.increaseQuantity = ()=>{
+            //setState form 1
+            //  this.setState({
+            //      qty: this.state.qty + 1
+            //  });
+             
+             //setState form 2
+             this.setState((prevState)=>{
+               return {
+                   qty: prevState.qty + 1
+               }
+             });
+        }
+        this.decreaseQuantity = ()=>{
+            const {qty} = this.state;
+            if(qty===0){
+                return;
+            }
+            //setState form 1
+            //  this.setState({
+            //      qty: this.state.qty + 1
+            //  });
+             
+             //setState form 2
+             this.setState((prevState)=>{
+               return {
+                   qty: prevState.qty - 1
+               }
+             });
         }
     }
     //for a class component to be a react component, we need one method -- render()
@@ -22,13 +54,28 @@ class CartItem extends React.Component{
                   <div className='right-block'>
                       <div style={{fontSize:25}}>{title}</div>
                       <div style={ { color: '#777'}}>Rs {price}</div>
-                      <div style={ { color: '#777'}}>Qty: {qty}</div>
+                      <div id = "quantity" style={ { color: '#777'}}>Qty: {qty}</div>
                   </div>
                   <div className='cart-item-actions'>
                        {/* {Button} */}
-                       <img alt='increase' className='action-icons' src='https://cdn-icons-png.flaticon.com/512/992/992651.png'></img>
-                       <img alt='decrease' className='action-icons' src='https://cdn-icons-png.flaticon.com/512/992/992683.png'></img>
-                       <img alt='increase' className='action-icons' src='https://cdn-icons.flaticon.com/png/512/484/premium/484662.png?token=exp=1646485732~hmac=ee9f0276fc1235bea31d9ec3ad26098a'></img>
+                       <img
+                        alt='increase' 
+                        className='action-icons'
+                        src='https://cdn-icons-png.flaticon.com/512/992/992651.png'
+                        onClick={this.increaseQuantity}
+                        />
+                       <img
+                        alt='decrease'
+                        className='action-icons'
+                        src='https://cdn-icons-png.flaticon.com/512/992/992683.png'
+                        onClick={this.decreaseQuantity}
+
+                        />
+                       <img
+                        alt='delete'
+                         className='action-icons'
+                          src='https://cdn-icons-png.flaticon.com/512/3096/3096673.png'
+                        />
                         
                   </div>
                   
